debbb2e64a211b6f8128eb2998c0c8b19e21160d
ncastro@vozy.co <ncastro@vozy.co>
2024-12-25 21:58:05 -0300

    finish UI login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,23 @@
-<h1>Login</h1>
+<h1 class="nombre-pagina">Login</h1>
+<p class="descripcion-pagina">Inicia sesión con tus datos</p>
+
+<from class="formulario" method="POST" action="/appbarberia/login">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" placeholder="Tu Email">
+    </div>
+    <div class="campo">
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" placeholder="Tu Password">
+    </div>
+
+    <input type="submit" class="boton" value="Iniciar Sesión">
+</from>
+
+<div class="acciones">
+
+    <a href="/appbarberia/crear-cuenta">¿Aún no tienes una cuenta? Crear una</a>
+    <a href="/appbarberia/forgot-password">¿Olvidaste tu Password?</a>
+
+
+</div>
